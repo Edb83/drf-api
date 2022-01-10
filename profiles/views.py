4a5df4cde877e3b343a33170a83f6135cd58b29f
Edb83 <ed3radley@gmail.com>
2022-01-10 21:15:27 +0000

    Add login/logout views, IsOwner permission and is_owner field to ProfileSerializer

--- profiles/views.py
+++ profiles/views.py
@@ -4,23 +4,32 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
 
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
+        serializer = ProfileSerializer(
+            profiles,
+            many=True,
+            context={
+                'request': request
+                },
+        )
         
         return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
     serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
 
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
 
         except Profile.DoesNotExist:
@@ -29,13 +38,25 @@ class ProfileDetail(APIView):
     def get(self, request, pk):
 
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
+        serializer = ProfileSerializer(
+            profile,
+            context={
+                'request': request
+                },
+            )
 
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
+        serializer = ProfileSerializer(
+            profile,
+            data=request.data,
+            context={
+                'request': request
+                },
+            )
+
         if serializer.is_valid():
             serializer.save()
 
