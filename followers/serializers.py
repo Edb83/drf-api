5734be3ef175962ce9edbbbca40cfadd21daa296
Edb83 <ed3radley@gmail.com>
2022-01-12 15:27:16 +0000

    Add followers app, inc model and FollowSerializer

---
+++ followers/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.db import IntegrityError
+from .models import Follower
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Follower model
+    Create method handles the unique constraint on 'owner' and 'followed'
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        model = Follower
+        fields = [
+            'id',
+            'owner',
+            'followed',
+            'followed_name',
+            'created_at',
+        ]
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'can only follow another user once'
+            })
