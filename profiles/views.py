93e3fbdeb4190bfdc8c71222a9b44768d9ff84da
Edb83 <ed3radley@gmail.com>
2022-01-28 14:45:49 +0000

    Add post and profle filters using DjangoFilterBackend

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -20,6 +21,11 @@ class ProfileList(generics.ListAPIView):
 
     filter_backends = [
         filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+
+    filterset_fields = [
+        'owner__following__followed__profile',
     ]
 
     ordering_fields = [
