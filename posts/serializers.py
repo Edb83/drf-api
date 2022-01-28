8d0c0166f1b86da94a2251ae4000a0a7a081deab
Edb83 <ed3radley@gmail.com>
2022-01-28 14:03:18 +0000

    Add comments_count and likes_count annotated fields to Post views; add related_name to post field in comment model

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,6 +9,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    comments_count = serializers.ReadOnlyField()
+    likes_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -54,4 +56,6 @@ class PostSerializer(serializers.ModelSerializer):
             'image',
             'image_filter',
             'like_id',
+            'comments_count',
+            'likes_count',
         ]
