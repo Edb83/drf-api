ac72c06711fbf6c54bea5bf726a1c4b19f25c94f
Edb83 <ed3radley@gmail.com>
2022-04-12 16:13:30 +0000

    Fix another indentation typo in CORS_ALLOWED_ORIGINS

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -75,8 +75,8 @@ ALLOWED_HOSTS = [
 # if 'CLIENT_ORIGIN' in os.environ:
 CORS_ALLOWED_ORIGINS = [
     os.environ.get('CLIENT_ORIGIN'),
-    os.environ.get('CLIENT_ORIGIN_DEV')
-    ]
+    os.environ.get('CLIENT_ORIGIN_DEV'),
+]
 
 # if 'CLIENT_ORIGIN_DEV' in os.environ:
 #     extracted_url = re.match(
